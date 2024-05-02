b428cfc62cb0626e39efe33e152dca0e50d7a269
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-01 22:06:24 -0300

    dash terminado

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 w-full h-full">
       Coders United ( LOGIN )
-      <Link href="/pages/dashboard/inicio">Inicio</Link>
+      <Link href="/pages/0/inicio">Inicio</Link>
     </main>
   );
 }
