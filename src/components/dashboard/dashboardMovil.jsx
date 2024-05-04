7233a1c6564ab5dc8d6ba083dc04d95c1a025f89
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-04 05:09:11 -0300

    iconos

--- src/components/dashboard/dashboardMovil.jsx
+++ src/components/dashboard/dashboardMovil.jsx
@@ -1,9 +1,12 @@
+
 "use client";
 import { IhuracanAmarillo } from "../../../public/iconos"
 import { useState } from "react"
 import { signOut } from "next-auth/react";
 import Link from "next/link";
-import { Icodigo, Ihuracan, Iproyecto, Iusuario, Iayuda, Iidea } from "../../../public/iconos";
+import { BiHomeAlt2 , BiFolder, BiCodeAlt, BiUser,  BiHelpCircle, BiLogOut } from "react-icons/bi";
+import { PiCubeFocusLight } from "react-icons/pi";
+
 
 export default function DashboardMovil(props){
     const ruta = props.ruta;
@@ -12,9 +15,9 @@ export default function DashboardMovil(props){
     const mostrarMenu = ()=>{
         mostrar ? setMostrar(false) : setMostrar(true);
     }
-    const titulos = [ "Inicio", "Proyectos", "Ideas", "Desarrolladores", "Perfil", "Ayuda"];
-    const enlaces = [ "inicio", "proyectos", "ideas", "desarrolladores", "perfil", "ayuda"];
-    const imagenes = [ Ihuracan, Iproyecto, Iidea, Icodigo,  Iusuario, Iayuda];
+    const titulos = [ "Inicio", "Proyectos", "Ideas", "Usuarios", "Perfil", "Ayuda"];
+    const enlaces = [ "inicio", "proyectos", "ideas", "usuarios", "perfil", "ayuda"];
+    const imagenes = [ <BiHomeAlt2 key="home"/>, <BiFolder key="Folder"/>,<PiCubeFocusLight key="FocusLight"/>, <BiCodeAlt key="CodeAlt"/>, <BiUser key="User"/>,  <BiHelpCircle key="HelpCircle"/>, <BiCodeAlt key="CodeAlt"/>];
     return(
         <div className={`${mostrar ? "menuActivo" : "menuOff" }`}>
             <div className="mt-2 flex justify-between ">
@@ -39,7 +42,7 @@ export default function DashboardMovil(props){
             {
                 titulos.map((e, index)=> (
                     <Link key={index} href={`/pages/${index}/${enlaces[index]}`} className={`flex p-2 space-x-3 text-slate-600 ${ruta == index && "fondo liston"}`}>
-                        <div className="scale-75 ml-4 mr-3">
+                        <div className="scale-150 mt-2 ml-4 mr-3">
                             {imagenes[index]}     
                         </div>
                         <span className={`${index == 2 || index == 0 ? "mt-2 text-sm" : "mt-1 text-sm" }`}>
@@ -49,8 +52,10 @@ export default function DashboardMovil(props){
                 ))
             }
                 </div>
-                <div className="text-neutral-500 ml-16 mt-3 text-sm">
-                    <button className="ml-3" onClick={async ()=> await signOut({callbackUrl:"/"})}>Cerrar sesión</button>
+                <div className="text-slate-600 ml-6 mt-4 text-sm">
+                    <button className=" flex" onClick={async ()=> await signOut({callbackUrl:"/"})}>
+                        <BiLogOut className="scale-150 mr-5" /> Cerrar sesión
+                    </button>
                 </div>
             </div>
             </div>
