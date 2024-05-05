dda3c66eda3ce7f5fcc28b3e31dea71b7b0dd4c7
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-05 06:47:46 -0300

    bugs

--- src/app/pages/(dashboard)/[name]/perfil/page.jsx
+++ src/app/pages/(dashboard)/[name]/perfil/page.jsx
@@ -1,11 +1,19 @@
 import ActualizarDatos from "../../../../../components/datosPersonales/actualizarDatos";
+import connectDB from "../../../../../libs/mongoose";
+import datosPerfil from "../../../../../models/datosPerfil";
 
-import { GET } from "../../../../api/datosPerfil/route";
+export async function GETE(){
+    await connectDB();
+    const res = await datosPerfil.find();
+    const data = await JSON.parse(JSON.stringify(res));
+
+    return data;
+}
 
 export default async function Perfil(){
 
 
-    const datos = await GET();
+    const datos = await GETE();
 
     return(
         <div>
