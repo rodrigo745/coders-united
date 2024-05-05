3616cbe521108b7e6f553bc010492c560ff6e4f8
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-04 21:14:44 -0300

    espaciado usuarios

--- src/app/pages/(dashboard)/[name]/usuarios/[...id]/page.jsx
+++ src/app/pages/(dashboard)/[name]/usuarios/[...id]/page.jsx
@@ -12,9 +12,18 @@ export default async function IdUsuario({params}){
     return (
         <div>
             <h3>Informacion del usuario</h3>
+            <br />
+
             <p>Nombre: {filtro.nombre} </p>
+            <br />
             <p>Correo: {filtro.correo} </p>
+            <br />
             <p>¿Tiene Imagen?: {filtro.nombre ? "SI" : "No"} </p>
+            <br />
+            <br />
+            <br />
+            <br />
+            <p>Modificar luego</p>
         </div>
     )
 }
