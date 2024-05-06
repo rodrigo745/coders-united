6717b221a50f2da0c2569852a5d009406aa67271
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-06 09:54:04 -0300

    respaldo

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
 "use client";
-import Link from "next/link";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-
-  const { data: session } = useSession();
 
   const router = useRouter();
 
