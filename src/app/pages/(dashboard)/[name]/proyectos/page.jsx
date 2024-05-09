f05ac4e8f6cb0b35b37af3446b90089249ad7f50
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-09 06:09:46 -0300

    contendor modal

--- src/app/pages/(dashboard)/[name]/proyectos/page.jsx
+++ src/app/pages/(dashboard)/[name]/proyectos/page.jsx
@@ -1,28 +1,16 @@
 import Image from "next/image"
-import { PiNotePencilBold } from "react-icons/pi";
 import ModalCrearProyecto from "../../../../../components/modalCrearProyecto/modalCrearProyecto";
 
-
 export default function Proyectos(){
+
     return(
         <div className="flex flex-col items-center">
             <div className="flex flex-col-reverse lg:flex lg:flex-row  cuadro w-[350px] lg:w-[500px] xl:w-[650px] h-[300px] rounded-lg p-2 py-4 lg:py-2"> { /* Boton crear */ }
-                <div className="h-full w-full justify-center  flex content-center items-center">
-                    <p className="flex justify-center amarilloBack shadow-md w-[250px]  h-fit font-bold text-black text-center p-2 rounded-full mt-5 mb-4 lg:mb-0 lg:mt-0 lg:ml-5">
-                        <PiNotePencilBold className="mt-1 scale-125"/>
-                        <span className="ml-2">
-                            Crear proyecto
-                        </span>
-                    </p>
-                </div>  
+                <ModalCrearProyecto/> 
                 <div className="flex justify-center w-full"> { /* imagen */ }
                     <Image src="/logo_proyectos.svg" width={240} height={240} alt="Logo de proyectos" className="scale-75 lg:scale-100" />
                 </div>
             </div>
-
-
-
-            <ModalCrearProyecto/>
 
         </div>
     )
--- src/components/modalCrearProyecto/modalCrearProyecto.jsx
+++ src/components/modalCrearProyecto/modalCrearProyecto.jsx
@@ -1,7 +1,33 @@
+"use client";
+import { useState } from "react";
+import { PiNotePencilBold } from "react-icons/pi";
+
 export default function ModalCrearProyecto(){
+
+    const [ mostrar, setMostrar ] = useState(false);
+
+    const mostrarModal = ()=> {
+        mostrar ? setMostrar(false) : setMostrar(true);
+    }
+
     return(
-        <div>
-            Crear Proyecto
-        </div>
+        <div  className="h-full w-full justify-center  flex content-center items-center ">
+            <p onClick={mostrarModal} className="flex justify-center amarilloBack shadow-md w-[250px]  h-fit font-bold text-black text-center p-2 rounded-full mt-5 mb-4 lg:mb-0 lg:mt-0 lg:ml-5 cursor-pointer">
+                <PiNotePencilBold className="mt-1 scale-125 z-10"/>
+                <span className="ml-2">
+                    Crear proyecto
+                </span>
+            </p>
+            {/* Modal */}
+            {
+                mostrar && 
+                    <div  className="modalContenedor z-30">
+                        <div onClick={mostrarModal} className="w-screen h-screen"></div>
+                        <div className="modalContenido rounded-lg w-[350px] md:w-[400px] lg:w-[600px] z-50">
+                            contenido
+                        </div>
+                    </div>
+            }
+        </div> 
     )
 }
