04741644453bd83eac56fc51920d3fa3beba030d
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-09 06:43:02 -0300

    s

---
+++ src/components/buscador/buscadorModalProyecto.jsx
@@ -0,0 +1,52 @@
+export default function BuscadorModalProyecto(){
+    return(
+        <div className="w-full">
+            <input type="search" placeholder="Agregar integrantes..." 
+                className="px-5 bg-slate-300 placeholder:text-neutral-700 text-black py-3 rounded-full w-full mt-5"/>
+            <div className="w-full mt-5 flex flex-wrap space-x-4">
+                <div className="bg-slate-500 w-fit p-1 px-3 rounded-full mb-3">
+                    <div className="absolute mt-[-13px] ml-[-17px] h-5 w-5 bg-red-400 rounded-full text-center">
+                        <span className="text-sm font-bold absolute ml-[-4px]">x</span>
+                    </div>
+                    <span>
+                        Rodrigo Quinteros
+                    </span>
+                </div>
+
+                <div className="bg-slate-500 w-fit p-1 px-3 rounded-full mb-3">
+                    <div className="absolute mt-[-13px] ml-[-17px] h-5 w-5 bg-red-400 rounded-full text-center">
+                        <span className="text-sm font-bold absolute ml-[-4px]">x</span>
+                    </div>
+                    <span>
+                        Rodrigo Quinteros
+                    </span>
+                </div>
+                <div className="bg-slate-500 w-fit p-1 px-3 rounded-full mb-3">
+                    <div className="absolute mt-[-13px] ml-[-17px] h-5 w-5 bg-red-400 rounded-full text-center">
+                        <span className="text-sm font-bold absolute ml-[-4px]">x</span>
+                    </div>
+                    <span>
+                        Rodrigo Quinteros
+                    </span>
+                </div>
+                <div className="bg-slate-500 w-fit p-1 px-3 rounded-full mb-3">
+                    <div className="absolute mt-[-13px] ml-[-17px] h-5 w-5 bg-red-400 rounded-full text-center">
+                        <span className="text-sm font-bold absolute ml-[-4px]">x</span>
+                    </div>
+                    <span>
+                        Rodrigo Quinteros
+                    </span>
+                </div>
+                <div className="bg-slate-500 w-fit p-1 px-3 rounded-full mb-3">
+                    <div className="absolute mt-[-13px] ml-[-17px] h-5 w-5 bg-red-400 rounded-full text-center">
+                        <span className="text-sm font-bold absolute ml-[-4px]">x</span>
+                    </div>
+                    <span>
+                        Rodrigo Quinteros
+                    </span>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
--- src/components/modalCrearProyecto/modalCrearProyecto.jsx
+++ src/components/modalCrearProyecto/modalCrearProyecto.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { PiNotePencilBold } from "react-icons/pi";
+import BuscadorModalProyecto from "../../components/buscador/buscadorModalProyecto";
 
 export default function ModalCrearProyecto(){
 
@@ -26,11 +27,19 @@ export default function ModalCrearProyecto(){
                         <div className="modalContenido rounded-lg w-[350px] md:w-[400px] lg:w-[600px] z-50 p-4">
                             <div className="flex flex-col">
                                 <div className="flex justify-between">
-                                    <p className="text-lg font-bold mt-1">Nuevo proyecto</p>
+                                    <p className="text-xl ml-2 font-bold mt-2">Nuevo proyecto</p>
                                     <p onClick={mostrarModal} className="h-9 w-14 text-2xl text-center font-bold bg-red-400 rounded-lg cursor-pointer">x</p>
                                 </div>
-                                <div>
+                                <div className="mt-6 text-md flex flex-col">
+                                    <input className="px-5 bg-slate-300 placeholder:text-neutral-700 text-black py-3 rounded-full w-full" placeholder="Nombre del proyecto" />
+
+                                    <input className="px-5 bg-slate-300 placeholder:text-neutral-700 text-black py-3 rounded-full w-full mt-5" placeholder="Campo adicional" />
+
                                     
+                                    <BuscadorModalProyecto/>
+
+                                    <label className="w-full amarilloBack text-black font-bold text-center p-2 rounded-full mt-5 text-lg">Crear</label>
+
                                 </div>
                             </div>
                         </div>
