0a7a32323684045886f7bc1e93112dfcb5b3b43c
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-11 22:16:09 -0300

    boton de crear

--- src/components/tareas/Columna.jsx
+++ src/components/tareas/Columna.jsx
@@ -17,14 +17,18 @@ export default function Columna(props){
     return(
         <div ref={setNodeRef} className="cuadro w-[20vw] rounded-lg h-[500px] p-4 flex flex-col items-center">
             <h4 className="text-lg">{props.titulo}</h4>
-            <div className='w-full flex flex-col h-full justify-between'>
-                <div className='overflow-auto'>
-                    <div className="h-fit min-h-56 w-full mt-2 flex flex-col-reverse  my-10 px-2 justify-end">
+            <div className='w-full flex flex-col h-[92%] justify-between space-y-2'>
+                <div className='overflow-auto h-full'>
+                    <div className="h-fit min-h-32 w-full mt-2 flex flex-col-reverse px-2 justify-end ">
                         {props.children}
                     </div>
                 </div>
-            <div className='w-full amarilloBack text-center p-2 rounded-full cursor-pointer'>
-                <p className='font-bold text-black '>Crear tarea</p>
+            <div className='w-full text-center flex space-x-2 px-2'>
+                <input type="text" placeholder={`Agregar tarea ${
+                    props.tablero == "progreso" ? "en progrso" : props.tablero}`} 
+                    className='p-2 bg-slate-600 rounded-full px-4 w-full'
+                />
+                <p className='font-bold text-black p-1 px-3 text-3xl amarilloBack rounded-full cursor-pointer'>+</p>
             </div>
             </div>
         </div>
