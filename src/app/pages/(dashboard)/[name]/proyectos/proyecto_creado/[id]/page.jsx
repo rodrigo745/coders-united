a7aa71851c6aceac6f84dcab7b71f9f4e43b0541
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-15 10:09:52 -0300

    a

--- src/app/pages/(dashboard)/[name]/proyectos/proyecto_creado/[id]/page.jsx
+++ src/app/pages/(dashboard)/[name]/proyectos/proyecto_creado/[id]/page.jsx
@@ -37,19 +37,19 @@ export default function IdProyecto({params}){
                 </div>
                 <div className="flex flex-col lg:flex lg:flex-row">
                     {/* navegacion del proyecto */}
-                    <div className="cuadro space-y-4 lg:w-[270px] h-fit p-3 py-5 mt-6 rounded-lg mx-8">
+                    <div className="cuadro space-y-4 lg:w-[270px] h-fit p-3 py-5 mt-6 rounded-lg mx-7 lg:mx-0">
                         <button className="amarilloBack h-fit w-[180px] lg:w-full text-black p-2 rounded-full font-bold ">Requerimientos</button>
-                        <button className="amarilloBack h-fit w-[180px] lg:w-full text-black p-2 rounded-full font-bold ml-3">Repositorio</button>
+                        <button className="amarilloBack h-fit w-[180px] lg:w-full text-black p-2 rounded-full font-bold ml-3 lg:ml-0">Repositorio</button>
                         <button className="amarilloBack w-[180px] lg:w-full text-black p-2 rounded-full font-bold">Contabilidad</button>
-                        <button className="amarilloBack w-[180px] lg:w-full text-black p-2 rounded-full font-bold ml-3">Diagramas</button>
+                        <button className="amarilloBack w-[180px] lg:w-full text-black p-2 rounded-full font-bold ml-3 lg:ml-0">Diagramas</button>
                         <button className="amarilloBack w-[180px] lg:w-full text-black p-2 rounded-full font-bold">Enlaces</button>
-                        <button className="amarilloBack w-[180px] lg:w-full text-black p-2 rounded-full font-bold ml-3">Evaluación</button>
+                        <button className="amarilloBack w-[180px] lg:w-full text-black p-2 rounded-full font-bold ml-3 lg:ml-0">Evaluación</button>
                         <button className="amarilloBack w-[180px] lg:w-full text-black p-2 rounded-full font-bold">Reuniones</button>
                     </div>
-                    <div className="cuadro w-full ml-7 mt-6 rounded-lg p-2">
+                    <div className="cuadro w-[89vw] min-h-36 lg:w-full ml-7 mt-6 rounded-lg p-2">
                         <div className="flex justify-between">
-                            <button className="amarilloBack w-[200px] text-black p-1.5 rounded-full font-bold">Notas</button>
-                            <button className="amarilloBack w-[200px] text-black p-1.5 rounded-full font-bold">Chat</button>
+                            <button className="amarilloBack w-[100px] lg:w-[200px] text-black p-1.5 rounded-full font-bold">Notas</button>
+                            <button className="amarilloBack w-[100px] lg:w-[200px] text-black p-1.5 rounded-full font-bold">Chat</button>
                         </div>
                         
                         {/* Contenido */}
