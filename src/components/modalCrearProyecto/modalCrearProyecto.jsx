7142dab6518b11f0fa3b8353737b5ff8e7c0ca3b
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-16 10:14:19 -0300

    envio de datos

--- src/components/modalCrearProyecto/modalCrearProyecto.jsx
+++ src/components/modalCrearProyecto/modalCrearProyecto.jsx
@@ -1,19 +1,53 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { PiNotePencilBold } from "react-icons/pi";
 import BuscadorModalProyecto from "../../components/buscador/buscadorModalProyecto";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { IoSearch } from "react-icons/io5";
 
 
 export default function ModalCrearProyecto(props){
 
     const usuarios = props.usuarios;
+    const router = useRouter();
     const [ mostrar, setMostrar ] = useState(false);
+    const [ valorMongo, setValorMongo ] = useState({});
+    // campos para los datos de mongo
+    const [ titulo, setTitulo ] = useState();
+    const [ descripcion, setDescripcion ] = useState();
+    const [ estado, setEstado ] = useState();
 
+    
     const mostrarModal = ()=> {
         mostrar ? setMostrar(false) : setMostrar(true);
     }
+
+
+    // modificacion de datos
+    useEffect(()=> {
+        setValorMongo({
+            titulo: titulo,
+            estado: "En desarrollo22",
+            descripcion: "asdasd"
+        })
+    },[titulo])
+
+    // enviar los datos aqui
+
+    const crearProyecto = async(e)=>{
+        e.preventDefault();
+        
+        const subir = await fetch(`/api/datosProyecto/asd`,{
+            method: "POST",
+            body: JSON.stringify(valorMongo),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        // router.push("./proyectos/proyecto_creado/as")
+    }
+
+    const get_titulo = (e)=> { setTitulo(e.target.value); console.log(e.target.value) }
 
     return(
         <div  className=" w-full justify-center  flex content-center items-center ">
@@ -35,7 +69,7 @@ export default function ModalCrearProyecto(props){
                                     <p onClick={mostrarModal} className="h-9 w-14 text-2xl text-center font-bold bg-red-400 rounded-lg cursor-pointer">x</p>
                                 </div>
                                 <div className="mt-6 text-md flex flex-col">
-                                    <input className="px-5 bg-slate-300 placeholder:text-neutral-700 text-black py-3 rounded-full w-full" placeholder="Nombre del proyecto *" />
+                                    <input onChange={get_titulo} className="px-5 bg-slate-300 placeholder:text-neutral-700 text-black py-3 rounded-full w-full" placeholder="Nombre del proyecto *" />
 
                                     <input className="px-5 bg-slate-300 placeholder:text-neutral-700 text-black py-3 rounded-full w-full mt-5" placeholder="Descripción" />
                                     
@@ -54,7 +88,7 @@ export default function ModalCrearProyecto(props){
                                     </div>
                                     <BuscadorModalProyecto usuarios={usuarios}/>
                                     {/* Cambiar por una funcion que guarde el proyecto y redirija */}
-                                    <Link href="./proyectos/proyecto_creado/as" className="w-full amarilloBack text-black font-bold text-center p-2 rounded-full mt-5 text-lg">Crear</Link>
+                                    <button onClick={crearProyecto} className="w-full amarilloBack text-black font-bold text-center p-2 rounded-full mt-5 text-lg">Crear</button>
 
                                 </div>
                             </div>
