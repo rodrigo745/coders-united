c2b6305c040c1aca1405c1a1de602374619ead89
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-17 03:15:47 -0300

    obtencion de datos

--- src/app/pages/(dashboard)/[name]/proyectos/proyecto_creado/[id]/page.jsx
+++ src/app/pages/(dashboard)/[name]/proyectos/proyecto_creado/[id]/page.jsx
@@ -1,10 +1,24 @@
-import Link from "next/link";
 import InfoGeneral from "../../../../../../../components/proyectoGeneral/infoGeneral";
 import CuadroVisual from "../../../../../../../components/proyectoGeneral/cuadroVisual";
 import SideBar from "../../../../../../../components/proyectoGeneral/sideBar";
+import { GET } from "../../../../../../api/datosProyecto/[id]/[individual]/route";
+// para acceder a los datos de mongo
+import proyecto from "../../../../../../../models/proyecto";
+import connectDB from "../../../../../../../libs/mongoose";
 
-export default function IdProyecto({params}){
+async function LoadDatos(id){
+    await connectDB;
+    const datos = await proyecto.findById(id);
+    const res = await JSON.parse(JSON.stringify(datos));
+    return datos;
+}
 
+export default async function IdProyecto({params}){
+
+    
+    const datosProyecto = await LoadDatos(params.id);
+
+    
     return(
         <div className="flex flex-col lg:flex lg:flex-row">
             <div className="flex flex-col  justify-center w-[100vw] lg:w-[60vw] items-center lg:block">
